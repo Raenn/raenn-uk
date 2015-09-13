140670af72b6ec093e504c677678717ca7e9bd59
Raenn <danbelsey@hotmail.co.uk>
2015-09-13 11:12:47 +0100

    add Snap.SVG dependency; add basic demo + remove the HTML/SCSS etc Yeoman generated

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,22 @@
-console.log('\'Allo \'Allo!');
+initStars();
+
+function initStars() {
+
+	var starWidth = 20;
+
+	var s = Snap(starWidth,starWidth);
+
+	var bigCircle = s.circle(starWidth / 2,starWidth / 2,starWidth / 2);
+	bigCircle.attr({
+		fill: 'r(0.5, 0.5, 0.5)#fff:0-rgba(255,255,255,128):25-rgba(255,255,255,0)',
+		strokeWidth: 0
+	})
+
+	bigCircle.hover(function() { console.log("hover in!") }, function() { console.log("hover out!") } )
+
+	//TODO: animate, as so, on hover
+	// var newGradient = s.gradient("r(0.5, 0.5, 0.5)#fff:0-rgba(255,255,255,128):25-rgba(255,255,255,0)");
+	// bigCircle.attr({
+	// 	fill: newGradient
+	// })
+}
